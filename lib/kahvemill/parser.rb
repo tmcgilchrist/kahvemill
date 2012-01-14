c085d21a87581d4e1ff5d6f423f61b85b8ad1983
Tim McGilchrist <timmcgil@gmail.com>
2012-01-15 10:17:30 +1100

    Basic parsing of numbers, exponents and fractions.

--- lib/kahvemill/parser.rb
+++ lib/kahvemill/parser.rb
@@ -2,9 +2,11 @@ require 'parslet'
 
 class KahveMill::Parser < Parslet::Parser
   rule(:number) do
-    integer.as(:integer)
+    integer.as(:integer) | fraction.as(:fraction) | exponent.as(:exponent)
   end
 
   rule(:integer) { match('[1-9]') >> match('[0-9]').repeat | match('0') }
+  rule(:fraction) { match('\.') >> match('[0-9]').repeat }
+  rule(:exponent) { match('[eE]') >> match('[-+]').maybe >> match('[0-9]').repeat }
   root(:number)
 end
--- spec/kahvemill/parser_spec.rb
+++ spec/kahvemill/parser_spec.rb
@@ -7,14 +7,34 @@ module KahveMill
       @parser = KahveMill::Parser.new
     end
 
-    describe 'hello' do
+    describe 'integer' do
       it 'accepts integer zero' do
-        @parser.parse("0")[:integer].should eql("0")
+        @parser.parse("0")[:integer].to_s.should eql("0")
       end
 
-      it 'accepts mulit-digit integer'
+      it 'accepts mulit-digit integer' do
+        @parser.parse("13")[:integer].to_s.should eql("13")
+      end
 
-      it 'accepts single-digit integer'
+      it 'accepts single-digit integer' do
+        @parser.parse("9")[:integer].to_s.should eql("9")
+      end
+    end
+
+    describe 'fraction' do
+      it 'accepts fraction' do
+        @parser.parse(".1")[:fraction].to_s.should eql(".1")
+      end
+    end
+
+    describe 'exponent' do
+      it 'accepts an exponent' do
+        exponents = ["e10", "E10", "e+123", "E+444", "e-1"]
+
+        exponents.each do |e|
+          @parser.parse(e)[:exponent].to_s.should eql(e)
+        end
+      end
     end
   end
 end
